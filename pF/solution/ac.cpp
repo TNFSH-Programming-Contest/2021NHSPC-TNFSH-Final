f778db1c1aa243d01c7b6ba7e0ad69517da9a1dc
enip2473 <enip2473@gmail.com>
2021-08-27 11:23:35 +0800

    add pF

--- pF/solution/ac.cpp
+++ pF/solution/ac.cpp
@@ -1,48 +1,96 @@
-// By xiplus
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+#define ll long long
+#define pii pair<ll,ll>
+#define int ll
+#define fi first
+#define se second
+#define vi vector<ll>
+#define pb emplace_back
+#define all(n) (n).begin(),(n).end()
+#define mem(n,x) memset(n,x,sizeof(n))
+#define IOS ios::sync_with_stdio(0),cin.tie(0)
+#define dbg(...) cerr<<#__VA_ARGS__<<" = ";_do(__VA_ARGS__);
+template<typename T> void _do(T x){cerr<<x<<"\n";}
+template<typename T,typename ...U> void _do(T x,U ...y){cerr<<x<<", ";_do(y...);}
+const ll mod = 1000000007;
 
-void add(long long a, long long b) {
-	cout << a << " + " << b << " = " << a + b << endl;
+ll n;
+#define mat array<array<ll, 10>, 10>
+mat trans[62];
+vi ans(10, 0);
+
+vi dp(vi v){
+    vi aa(10, 0);
+    aa[0] = v[0];
+    for(int i = 1; i < 10; i++) aa[i] = (aa[i-1] + v[i]) % mod;
+    return aa;
 }
-void sub(long long a, long long b) {
-	cout << a << " - " << b << " = " << a - b << endl;
-}
-void mul(long long a, long long b) {
-	cout << a << " * " << b << " = " << a * b << endl;
-}
-void div2(long long a, long long b) {
-	cout << a << " / " << b << " = " << a / b << " ... " << a % b << endl;
-}
-typedef void (*ScriptFunction)(long long, long long);
 
-struct div_0_exception {};
-
-void error(int) {
-	throw div_0_exception();
+vi vec_mul(vi v, mat m){
+    vi aa(10, 0);
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < 10; j++){
+            aa[i] += v[j] * m[j][i];
+            aa[i] %= mod;
+        }
+    }
+    return aa;
 }
 
-int main() {
-	if (SIG_ERR == signal(SIGFPE, error)) {
-		cerr << "failure to setup signal.";
-		return 1;
-	}
-
-	map<char, ScriptFunction> func;
-	func['+'] = add;
-	func['-'] = sub;
-	func['*'] = mul;
-	func['/'] = div2;
-
-	long long a, b;
-	char c;
-	cin >> a >> c >> b;
-	cout << fixed << setprecision(4);
-	try {
-		func[c](a, b);
-	} catch (div_0_exception) {
-		cout << "ERROR" << endl;
-	}
-
-	return 0;
+mat operator*(mat x, mat y){
+    mat aa;
+    for(int i = 0; i < 10; i++) for(int j = 0; j < 10; j++) aa[i][j] = 0;
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < 10; j++){
+            for(int k = 0; k < 10; k++){
+                aa[i][j] += x[i][k] * y[k][j];
+                aa[i][j] %= mod;
+            }
+        }
+    }
+    return aa;
 }
+
+mat mat_mul(mat x, ll p){
+    mat m = x, aa;
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < 10; j++){
+            aa[i][j] = 0;
+        }
+    }
+    for(int i = 0; i < 10; i++) aa[i][i] = 1;
+    while(p){
+        if(p & 1) aa = aa * m;
+        m = m * m;
+        p >>= 1;
+    }
+    return aa;
+}
+
+signed main(){
+    IOS;
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < 10; j++){
+            trans[0][i][j] = 10 - max(i, j);
+        }
+    }
+    for(int i = 1; i < 60; i++) trans[i] = trans[i-1] * trans[i-1];
+    cin >> n;
+    fill(all(ans), 0);
+    for(int i = 0; i < 10; i++) ans[i] = 1;
+    for(ll i = 0, d = (n - 1) / 2; i < 60; i++){
+        if(d >> i & 1) ans = vec_mul(ans, trans[i]);
+    }
+    if(~n & 1) ans = dp(ans);
+    ll cnt = 0;
+    for(int i = 0; i < 10; i++) cnt += ans[i], cnt %= mod;
+    cout << cnt << '\n';
+}
+
+/*
+1
+5 2
+2 6
+4 5
+*/
--- pF/validator/validator.cpp
+++ pF/validator/validator.cpp
@@ -6,14 +6,7 @@ using namespace std;
 int main() {
 	registerValidation();
 
-	int A = inf.readInt(-1000000000, 1000000000);
-	inf.readSpace();
-	inf.readChar();
-	inf.readSpace();
-	int B = inf.readInt(-1000000000, 1000000000);
+	long long A = inf.readLong(1, (long long) 1e18);
 	inf.readEoln();
-
 	inf.readEof();
-
-	return 0;
 }
