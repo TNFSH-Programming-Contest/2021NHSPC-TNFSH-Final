00ff29f4ffa18fe5e0ff1f49b0405bb235bd639c
iris2617 <53460413+iris2617@users.noreply.github.com>
2021-09-17 21:37:27 +0800

    add ac.cpp

--- pC/solution/ac.cpp
+++ pC/solution/ac.cpp
@@ -1,48 +1,104 @@
-// By xiplus
-#include <bits/stdc++.h>
-using namespace std;
+#include<bits/stdc++.h>
+#define int long long
+#define matsuri pair<int,int>
+using namespace std; 
+const int iris = 998244353;
 
-void add(long long a, long long b) {
-	cout << a << " + " << b << " = " << a + b << endl;
-}
-void sub(long long a, long long b) {
-	cout << a << " - " << b << " = " << a - b << endl;
-}
-void mul(long long a, long long b) {
-	cout << a << " * " << b << " = " << a * b << endl;
-}
-void div2(long long a, long long b) {
-	cout << a << " / " << b << " = " << a / b << " ... " << a % b << endl;
-}
-typedef void (*ScriptFunction)(long long, long long);
+int arr[1010][1010],dis[3][1010][1010],aoi[1010][1010];
+int dir[][2]={{1,0},{-1,0},{0,1},{0,-1}};
+int n,m;
+#define sagiri pair<int,matsuri>
+priority_queue<sagiri, vector<sagiri>, greater<sagiri> > pq;
 
-struct div_0_exception {};
-
-void error(int) {
-	throw div_0_exception();
-}
-
-int main() {
-	if (SIG_ERR == signal(SIGFPE, error)) {
-		cerr << "failure to setup signal.";
-		return 1;
+void dijk(int a,int b,int k)
+{
+	int i,j,c,d;
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=m;j++)
+		{
+			dis[k][i][j]=1e18;
+		}
 	}
-
-	map<char, ScriptFunction> func;
-	func['+'] = add;
-	func['-'] = sub;
-	func['*'] = mul;
-	func['/'] = div2;
-
-	long long a, b;
-	char c;
-	cin >> a >> c >> b;
-	cout << fixed << setprecision(4);
-	try {
-		func[c](a, b);
-	} catch (div_0_exception) {
-		cout << "ERROR" << endl;
+	dis[k][a][b]=arr[a][b];
+	pq.push({dis[k][a][b], {a,b}});
+	while(!pq.empty())
+	{
+		a=pq.top().second.first;
+		b=pq.top().second.second;
+		c=pq.top().first;
+		pq.pop();
+		if(dis[k][a][b]!=c)
+		{
+			continue;
+		}
+		for(i=0;i<4;i++)
+		{
+			c=a+dir[i][0];
+			d=b+dir[i][1];
+			if(c<1 || c>n || d<1 || d>m)
+			{
+				continue;
+			}
+			if(dis[k][a][b]+arr[c][d]<dis[k][c][d])
+			{
+				dis[k][c][d]=dis[k][a][b]+arr[c][d];
+				pq.push({dis[k][c][d], {c,d}});
+			}
+		}
 	}
+}
 
+signed main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int i,j,ans=1e18,akari,x,y,ex,ey,sx,sy,nene;
+	akari=0;
+	x=y=1;
+	
+	cin>>n>>m;
+	cin>>sx>>sy;
+	cin>>ex>>ey;
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=m;j++)
+		{
+			cin>>arr[i][j];
+			if(arr[i][j]<0)
+			{
+				akari=arr[i][j];
+				arr[i][j]=0;
+				x=i;
+				y=j;
+			}
+		}
+	}
+	dijk(sx,sy,0);
+	dijk(x,y,1);
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=m;j++)
+		{
+			aoi[i][j]=arr[i][j]+max(0LL, arr[i][j]+akari);
+			arr[i][j]=max(0LL, arr[i][j]+akari);
+		}
+	}
+	dijk(ex,ey,2);
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=m;j++)
+		{
+			nene=dis[0][i][j]+dis[1][i][j]+dis[2][i][j]-aoi[i][j];
+			if(nene<=ans)
+			{
+				ans=nene;
+			}
+		}
+	}
+	ans=min(ans, dis[0][ex][ey]);
+	cout<<ans<<'\n';
+	
 	return 0;
 }
