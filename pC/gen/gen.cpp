ad07838d881cba3d7a2f17a65e4e5fca432f9cdd
iris2617 <53460413+iris2617@users.noreply.github.com>
2021-09-17 21:15:57 +0800

    add gen.cpp

--- pC/gen/gen.cpp
+++ pC/gen/gen.cpp
@@ -1,39 +1,97 @@
-#include <bits/stdc++.h>
 #include "testlib.h"
-#define endl '\n'
-using namespace std;
-char sign[] = {'+', '-', '*', '/'};
+#include<bits/stdc++.h>
+//#define int long long
+#define matsuri pair<int,int>
+using namespace std; 
+const int iris = 1e9+7;
 
-void gen_task2() {
-	int signid = rnd.next(4);
-	int B = rnd.next(1, 10000);
-
-	cout << 0 << " " << sign[signid] << " " << B << endl;
+bool check(int a,int b,int c,int d,int e,int f)
+{
+	int aa,bb,cc,dd;
+	aa=min(a,c);
+	bb=min(b,d);
+	cc=max(a,c);
+	dd=max(b,d);
+	return aa<=e && e<=cc && bb<=f && f<=dd;
 }
 
-void gen_norm(long long MIN_V, long long MAX_V) {
-	int A = rnd.next(MIN_V, MAX_V);
-	int signid = rnd.next(4);
-	int B = rnd.next(MIN_V, MAX_V);
-
-	cout << A << " " << sign[signid] << " " << B << endl;
-}
-
-int main(int argc, char* argv[]) {
+signed main(int argc, char *argv[])
+{
 	ios::sync_with_stdio(0);
 	cin.tie(0);
+	
 	registerGen(argc, argv, 1);
-
-	string MODE = argv[1];
-
-	if (MODE == "norm") {
-		long long MIN_V = atoll(argv[2]);
-		long long MAX_V = atoll(argv[3]);
-
-		gen_norm(MIN_V, MAX_V);
-	} else if (MODE == "task2") {
-		gen_task2();
-	} else {
-		return -1;
+	
+	int n,m,a,b,c,d,e,f,i,j,aoi;
+	int s,ss;
+	s=atoi(argv[1]);
+	ss=atoi(argv[2]);
+	
+	if(s==3)
+	{
+		n=m=4;
 	}
+	else
+	{
+		n=rnd.wnext(1000,10)+1;
+		m=rnd.wnext(1000,10)+1;
+	}
+	cout<<n<<' '<<m<<'\n';
+	
+	a=rnd.next(1,n);
+	b=rnd.next(1,m);
+	do
+	{
+		c=rnd.next(1,n);
+		d=rnd.next(1,m);
+	}
+	while(a==c && b==d);
+	if(s==2 || s==4)
+	{
+		do
+		{
+			e=rnd.next(1,n);
+			f=rnd.next(1,m);
+		}
+		while((a==e && b==f) || (c==e && d==f) || ((check(a,b,c,d,e,f)^(ss<=4))));
+	}
+	else
+	{
+		do
+		{
+			e=rnd.next(1,n);
+			f=rnd.next(1,m);
+		}
+		while((a==e && b==f) || (c==e && d==f));
+	}
+	cout<<a<<' '<<b<<'\n'<<c<<' '<<d<<'\n';
+	
+	if(s==1)
+		e=f=-1;
+	if(s==2)
+		aoi=rnd.next(0,1000000000);
+	
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=m;j++)
+		{
+			if(s==2)
+				a=aoi;
+			else
+				a=rnd.next(0,1000000000);
+			if(i==e && j==f)
+			{
+				a=rnd.next(0,1000000000);
+				if(s==2)
+				{
+					a=min(a, aoi-1);
+				}
+				a=-a;
+			}
+			cout<<a<<" \n"[j==m];
+		}
+	}
+	
+	
+	return 0;
 }
