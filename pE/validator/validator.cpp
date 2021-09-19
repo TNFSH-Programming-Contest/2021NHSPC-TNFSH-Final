0378e19a28e981643f3607caf7f8d13d7faf6996
xiplus <huangxuanyuxiplus@gmail.com>
2021-09-19 08:39:50 +0800

    pE Add validator

--- pE/validator/validator.cpp
+++ pE/validator/validator.cpp
@@ -1,3 +1,30 @@
+#include <bits/stdc++.h>
+
+#include "testlib.h"
+
+using namespace std;
+
 int main() {
+	registerValidation();
+
+	inf.readInt(-1000000000, 1000000000);
+	inf.readSpace();
+	inf.readInt(-1000000000, 1000000000);
+	inf.readSpace();
+	inf.readInt(-1000000000, 1000000000);
+	inf.readEoln();
+
+	inf.readInt(-1000000000, 1000000000);
+	inf.readSpace();
+	inf.readInt(-1000000000, 1000000000);
+	inf.readEoln();
+
+	inf.readLong(1, (long long)1e18);
+	inf.readSpace();
+	inf.readInt(1, 10000);
+	inf.readEoln();
+
+	inf.readEof();
+
 	return 0;
 }
