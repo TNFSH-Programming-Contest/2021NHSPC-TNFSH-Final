41e182dca7788688b8796dc0436c096dbd723f77
xiplus <huangxuanyuxiplus@gmail.com>
2021-09-19 22:52:44 +0800

    Restore pA validator

--- pA/validator/validator.cpp
+++ pA/validator/validator.cpp
@@ -1,4 +1,24 @@
-int main()
-{
+#include <bits/stdc++.h>
+
+#include "testlib.h"
+
+using namespace std;
+
+int main() {
+	registerValidation();
+
+	int n = inf.readInt(1, 8000);
+	cout << n << endl;
+	inf.readEoln();
+
+	inf.readInt(1, 1000000000);
+	for (int i = 1; i < n; i++) {
+		inf.readSpace();
+		inf.readInt(1, 1000000000);
+	}
+	inf.readEoln();
+
+	inf.readEof();
+
 	return 0;
 }
