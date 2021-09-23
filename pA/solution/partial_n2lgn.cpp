d0c35ab2e18bf983309b869d4c1503f4fb7b6acb
Li-Pro <53344765+Li-Pro@users.noreply.github.com>
2021-09-23 22:38:15 +0800

    Add pA test solutions.

---
+++ pA/solution/partial_n2lgn.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n;
+    cin >> n;
+    
+    vector<int> v(n);
+    for (int i=0; i<n; i++)
+    {
+        cin >> v[i];
+    }
+    
+    map<long long, int> prfidx;
+    for (int i=0; i<n; i++)
+    {
+        long long S = accumulate(v.begin(), next(v.begin() + i), 0LL);
+        prfidx[S] = i;
+    }
+    
+    auto test = [&](long long k) -> int
+    {
+        int mxslen = 0;
+        long long S = accumulate(v.begin(), v.end(), 0LL);
+        if (S % k != 0)
+        {
+            return -1;
+        }
+        
+        {
+            int lidx = -1;
+            for (long long ps=k; ps<=S; ps+=k)
+            {
+                if (!prfidx.count(ps))
+                {
+                    return -1;
+                }
+                
+                mxslen = max(mxslen, prfidx[ps] - lidx);
+                lidx = prfidx[ps];
+            }
+        }
+        
+//        for (long long lidx=-1, ps=k; ps<=S; ps+=k)
+//        {
+//            cout << "##" << lidx+2 << " " << prfidx[ps]+1 << '\n';
+//            lidx = prfidx[ps];
+//        }
+//        cout << "###\n";
+        
+        return mxslen;
+    };
+    
+    int mnlen = n;
+    for (int i=0; i<n; i++)
+    {
+        long long S = accumulate(v.begin(), next(v.begin() + i), 0LL);
+        
+        int len = test(S);
+        if (len > 0)
+        {
+            mnlen = min(mnlen, len);
+        }
+    }
+    
+    cout << mnlen << '\n';
+}
---
+++ pA/solution/partial_n3.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n;
+    cin >> n;
+    
+    vector<int> v(n);
+    for (int i=0; i<n; i++)
+    {
+        cin >> v[i];
+    }
+    
+    auto test = [&](long long k) -> int
+    {
+        int mxslen = 0;
+        for (int i=0, j=0; i<n; i = j)
+        {
+            long long S = 0;
+            for (j=i; j<n && S<k; j++)
+            {
+                S += v[j];
+            }
+            
+            if (S != k)
+            {
+                return -1;
+            }
+            else
+            {
+                mxslen = max(mxslen, j - i);
+            }
+        }
+        
+        return mxslen;
+    };
+    
+    int mnlen = n;
+    for (int i=0; i<n; i++)
+    {
+        for (int j=i; j<n; j++)
+        {
+            long long S = accumulate(v.begin() + i, next(v.begin() + j), 0LL);
+            
+            int len = test(S);
+            if (len > 0)
+            {
+                mnlen = min(mnlen, len);
+            }
+        }
+    }
+    
+    cout << mnlen << '\n';
+}
